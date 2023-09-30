90956ee7d45143ec609de567c1490e1fbc3f1bbc
nmbv7 <nmbv90@gmail.com>
2023-09-30 16:11:13 +0100

    falta perceber porque adição está com erro

---
+++ Calaculadora JavaScript/script.js
@@ -0,0 +1,31 @@
+function add()
+{
+        n1 = document.getElementById("num1").value;
+        n2 = document.getElementById("num2").value;
+        document.getElementById("result").innerHTML = n1 + n2;
+}
+
+function sub()
+{
+        n1 = document.getElementById("num1").value;
+        n2 = document.getElementById("num2").value;
+        document.getElementById("result").innerHTML = n1 - n2;
+}
+
+function mult()
+{
+        n1 = document.getElementById("num1").value;
+        n2 = document.getElementById("num2").value;
+        document.getElementById("result").innerHTML = n1 * n2;
+}
+
+function divi()
+{
+        n1 = document.getElementById("num1").value;
+        n2 = document.getElementById("num2").value;
+        document.getElementById("result").innerHTML = n1 / n2;
+}
+
+function reset(){
+    window.location.reload()
+}
