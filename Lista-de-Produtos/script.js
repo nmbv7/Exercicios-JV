352d0548e3ad2bc7cbdb106e7d40ccc04613599d
nmbv7 <nmbv90@gmail.com>
2023-09-30 18:29:23 +0100

    Exercicio pronto - falta melhorar apresentaçao

--- Lista-de-Produtos/script.js
+++ Lista-de-Produtos/script.js
@@ -14,4 +14,7 @@ var produto2 = new Produtos ('Samsung', 'telemoveis', 1200);
 var produto3 = new Produtos ('MSI', 'computadores', 2000);
 var produto4 = new Produtos ('Acer', 'Computadores', 2500);
 
-document.getElementById("p1").innerHTML = Produtos;
+document.getElementById("p1").innerHTML = Produtos.app;
+
+
+
