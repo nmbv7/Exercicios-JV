a580c6d9e5b124d9f8434fe9c5d0c34a088b0b87
nmbv7 <nmbv90@gmail.com>
2023-10-01 11:39:27 +0100

    adicionei um alert, tinha o resultado num <p>

--- Gerar número-random-de-1-a-10/script.js
+++ Gerar número-random-de-1-a-10/script.js
@@ -1,11 +1,8 @@
 
-var randomNr = Math.floor((Math.random() * 10) + 1);
 
 function getRandomNr(){
-    document.getElementById('demo').innerHTML = randomNr
+    var randomNr = Math.floor((Math.random() * 10) + 1);
+    alert(randomNr) 
 }
 
-function reset(){
-    window.location.reload()
-}
 
